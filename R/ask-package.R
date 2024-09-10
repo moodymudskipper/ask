b7f620a180caadd03606e87d9b7c6a3cf196fa55
Antoine Fabri <antoine.fabri@gmail.com>
2024-09-10 13:22:37 +0200

    import more functions

--- R/ask-package.R
+++ R/ask-package.R
@@ -1,6 +1,7 @@
 #' @keywords internal
 #' @importFrom rlang abort inform warn %||% arg_match
 #' @importFrom utils capture.output sessionInfo download.file
+#' @importFrom shiny observeEvent req updateSelectInput HTML
 "_PACKAGE"
 
 ## usethis namespace: start
